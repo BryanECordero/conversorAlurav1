182e366e27c30a4681a178e8834b506d1d5b7f9a
Bryan Cordero <bryan_cp11@hotmail.com>
2023-07-05 17:20:51 -0600

    Implement conversion method

--- src/Masa.java
+++ src/Masa.java
@@ -1,6 +1,63 @@
 public class Masa {
-    private static String[] valores = {"kg", "lb", "g"};
-    public static String[] getValores(){
+    private static String[] valores = { "kg", "lb", "g", "oz", "Ton metrica", "Ton corta" };
+
+    public static String[] getValores() {
         return valores;
+    }
+
+    public static double convertir(Double amount, String fromUnit, String toUnit) {
+        double convertedAmount = 0.0;
+        // Convertir la cantidad a gramos como unidad base
+        double grams = 0.0;
+        switch (fromUnit.toLowerCase()) {
+            case "kg":
+                grams = amount * 1000;
+                break;
+            case "g":
+                grams = amount;
+                break;
+            case "lb":
+                grams = amount * 453.59237;
+                break;
+            case "ton metrica":
+                grams = amount * Math.pow(10, 6);
+                break;
+            case "ton corta":
+                grams = amount * 907185;
+                break;
+            case "oz":
+                grams = amount * 28.3495;
+                break;
+            default:
+                // System.out.println("Unidad de masa no reconocida: " + fromUnit);
+                return convertedAmount;
+        }
+
+        // Convertir los gramos a la unidad de destino
+        switch (toUnit.toLowerCase()) {
+            case "kg":
+                convertedAmount = grams / 1000;
+                break;
+            case "g":
+                convertedAmount = grams;
+                break;
+            case "lb":
+                convertedAmount = grams / 453.59237;
+                break;
+            case "ton metrica":
+                convertedAmount = grams / Math.pow(10, 6);
+                break;
+            case "ton corta":
+                convertedAmount = grams / 907185;
+                break;
+            case "oz":
+                convertedAmount = grams / 28.3495;
+                break;
+            default:
+                // System.out.println("Unidad de masa no reconocida: " + toUnit);
+                return convertedAmount;
+        }
+
+        return convertedAmount;
     }
 }
--- src/Temperatura.java
+++ src/Temperatura.java
@@ -1,6 +1,45 @@
 public class Temperatura {
-    private static String[] valores = {"C", "K", "F"};
-    public static String[] getValores(){
+    private static String[] valores = { "°C", "K", "°F" };
+
+    public static String[] getValores() {
         return valores;
+    }
+
+    public static double convertir(Double amount, String fromUnit, String toUnit) {
+        double convertedAmount = 0.0;
+        double gradosC = 0.0;
+        // Converitr a la unidad base C
+        switch (fromUnit) {
+            case "°C":
+                gradosC = amount;
+                break;
+            case "K":
+                gradosC = amount - 273.15;
+                break;
+            case "°F":
+                gradosC = (amount - 32) / 1.8;
+                break;
+            default:
+                // System.out.println("Unidad de masa no reconocida: " + fromUnit);
+                return convertedAmount;
+        }
+
+        // Convertir los grado centigrados a la unidad de destino
+        switch (toUnit) {
+            case "°C":
+                convertedAmount = gradosC;
+                break;
+            case "K":
+                convertedAmount = gradosC + 273.15;
+                break;
+            case "°F":
+                convertedAmount = (gradosC * 1.8) + 32;
+                break;
+            default:
+                // System.out.println("Unidad de masa no reconocida: " + toUnit);
+                return convertedAmount;
+        }
+
+        return convertedAmount;
     }
 }
