34af7425c1bebeb6784affe74bb7b356a2540976
Bryan Cordero <bryan_cp11@hotmail.com>
2023-07-05 17:24:48 -0600

    Setup cards views

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,13 +1,30 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 public class Calculadora {
 
-    public static double convertir(String tipo, String valor,Object selectedItem, Object selectedItem2) {
-        try{
-            Double cantidad = Double.parseDouble(valor);
-            return cantidad;
-        }catch(NumberFormatException e){
-            return 0; 
-        }
-    }
+    public static double convertirUnidades(String className, String methodName, Object... parameters)
+            throws ClassNotFoundException, NoSuchMethodException, SecurityException, InstantiationException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        // Obtener la clase por su nombre
+        Class<?> clazz = Class.forName(className);
 
+        // Obtener el constructor sin argumentos y crear una instancia de la clase
+        Constructor<?> constructor = clazz.getDeclaredConstructor();
+        Object instance = constructor.newInstance();
+
+        // Obtener el método por su nombre y tipos de parámetros
+        Class<?>[] parameterTypes = new Class<?>[parameters.length];
+        for (int i = 0; i < parameters.length; i++) {
+            parameterTypes[i] = parameters[i].getClass();
+        }
+        Method method = clazz.getDeclaredMethod(methodName, parameterTypes);
+
+        // Invocar el método en la instancia creada, pasando los parámetros
+        Object result = method.invoke(instance, parameters);
+
+        // Imprimir el resultado
+        return (double) result;
+    }
 }
